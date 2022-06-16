fc63f30b70695e50e1a95fc2dd9d1e157a3dadf0
Samet732 <samethub732@gmail.com>
2022-06-16 18:24:41 +0300

    Create Storage.py

---
+++ Storage.py
@@ -0,0 +1,27 @@
+import sqlite3 as sq
+from Book import *
+
+class Storage():
+    def __init__(self, filename: str) -> None:
+        self.db = sq.connect(filename)
+        self.cursor = self.db.cursor()
+        self.cursor.execute('CREATE TABLE IF NOT EXISTS Book(Code TEXT, Name TEXT, Writer TEXT, Year INTEGER, Piece INTEGER, Price REAL)')
+
+    def add(self, b: Book) -> None:
+        self.cursor.execute(f'INSERT INTO Book VALUES("{b.code}", "{b.name}", "{b.writer}", "{b.year}", "{b.piece}", "{b.price}")')
+        self.db.commit()
+
+    def update(self, b: Book) -> None:
+        self.cursor.execute(f'UPDATE Book SET Name="{b.name}", Writer="{b.writer}", Year="{b.year}", Piece="{b.piece}", Price="{b.price}" WHERE Code="{b.code}"')
+        self.db.commit()
+
+    def delete(self, code: str) -> None:
+        self.cursor.execute(f'DELETE FROM Book WHERE Code="{code}"')
+        self.db.commit()
+
+    def getAll(self) -> list:
+        self.cursor.execute('SELECT * FROM Book')
+        return self.cursor.fetchall()
+
+    def close(self) -> None:
+        self.db.close()

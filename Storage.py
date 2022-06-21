65e357f9271aa67a204b003bc4e2628608782c68
Samet732 <samethub732@gmail.com>
2022-06-21 18:45:58 +0300

    renamed variable

--- Storage.py
+++ Storage.py
@@ -5,14 +5,14 @@ class Storage():
     def __init__(self, filename: str) -> None:
         self.db = sq.connect(filename)
         self.cursor = self.db.cursor()
-        self.cursor.execute('CREATE TABLE IF NOT EXISTS Book(Code TEXT, Name TEXT, Writer TEXT, Year INTEGER, Piece INTEGER, Price REAL)')
+        self.cursor.execute('CREATE TABLE IF NOT EXISTS Book(Code TEXT, Name TEXT, Author TEXT, Year INTEGER, Piece INTEGER, Price REAL)')
 
     def add(self, b: Book) -> None:
-        self.cursor.execute(f'INSERT INTO Book VALUES("{b.code}", "{b.name}", "{b.writer}", "{b.year}", "{b.piece}", "{b.price}")')
+        self.cursor.execute(f'INSERT INTO Book VALUES("{b.code}", "{b.name}", "{b.author}", "{b.year}", "{b.piece}", "{b.price}")')
         self.db.commit()
 
     def update(self, b: Book) -> None:
-        self.cursor.execute(f'UPDATE Book SET Name="{b.name}", Writer="{b.writer}", Year="{b.year}", Piece="{b.piece}", Price="{b.price}" WHERE Code="{b.code}"')
+        self.cursor.execute(f'UPDATE Book SET Name="{b.name}", Author="{b.author}", Year="{b.year}", Piece="{b.piece}", Price="{b.price}" WHERE Code="{b.code}"')
         self.db.commit()
 
     def delete(self, code: str) -> None:

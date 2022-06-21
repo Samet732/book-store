4e24faea7aa378e6e2eff1e3de4b1507e7803647
Samet732 <samethub732@gmail.com>
2022-06-21 18:45:17 +0300

    added function bookToTuple

--- Book.py
+++ Book.py
@@ -1,17 +1,21 @@
 import uuid
 
 class Book():
-    def __init__(self, name: str, writer: str, year: int, piece: int, price: float) -> None:
+    def __init__(self, name: str, author: str, year: int, piece: int, price: float) -> None:
         self.code = uuid.uuid4()
         self.name = name
-        self.writer = writer
+        self.author = author
         self.year = year
         self.piece = piece
         self.price = price
-        
-
-def tuppleToBook(tup) -> Book:
-    b = Book(tup[1], tup[2], tup[3], tup[4], tup[5])
-    b.code = tup[0]
     
-    return b
+    @classmethod
+    def tupleToBook(cls, tup):
+        b = Book(tup[1], tup[2], tup[3], tup[4], tup[5])
+        b.code = tup[0]
+    
+        return b
+
+    @classmethod
+    def bookToTuple(cls, b) -> tuple:
+        return (b.code, b.name, b.author, b.year, b.piece, b.price)
